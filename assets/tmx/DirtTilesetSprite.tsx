0db07ceb76129995b629fc8c6ca23cd937bd580f
Paolo Mazzon <pdmazzon@gmail.com>
2020-04-07 16:13:20 -0700

    Rearranged directory and added a starting room

---
+++ assets/tmx/DirtTilesetSprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="DirtTilesetSprite" tilewidth="8" tileheight="8" tilecount="48" columns="48">
+ <image source="../dirttileset.png" width="384" height="8"/>
+</tileset>
---
+++ assets/tmx/Entities.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Entities" tilewidth="8" tileheight="16" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="7" height="12" source="../playerstatic.png"/>
+ </tile>
+ <tile id="2">
+  <image width="8" height="16" source="../skellymanstatic.png"/>
+ </tile>
+</tileset>
